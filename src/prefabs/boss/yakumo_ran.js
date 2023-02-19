6bbf127cc97129543e70a7808b6827f43474ed8f
BlueWhaleMain <bluewhalemain@outlook.com>
2023-02-19 15:56:46 +0800

    为boss“八云蓝”补充隐藏材质

--- src/prefabs/boss/yakumo_ran.js
+++ src/prefabs/boss/yakumo_ran.js
@@ -9,9 +9,14 @@ const cache = document.createElement("canvas");
 cache.width = 128;
 cache.height = 128;
 const cacheCtx = cache.getContext("2d");
+const hide = document.createElement("canvas");
+hide.width = 128;
+hide.height = 128;
+const hideCtx = hide.getContext("2d");
 const bossAll = newImage(resources.Images.bossAll);
 bossAll.addEventListener("load", function () {
     cacheCtx.drawImage(bossAll, 128 * 2, 128 * 6, 128, 128, 0, 0, 128, 128)
+    hideCtx.drawImage(bossAll, 128 * 3, 128 * 6, 128, 128, 0, 0, 128, 128)
 });
 const texture = newImage(resources.Images.bossYakumoRan);
 const layerStage = getLayer(LAYER_MAPPING.STAGE);
@@ -24,7 +29,11 @@ export default function bossYakumoRan(x, y, blood, cards, dialogue) {
     inst.bgmName = ASSETS.SOUND.aMaidensIllusionaryFuneralNecroFantasy.name;
     inst.addLayer("YakumoRan", function () {
         this.draw = function (inst) {
-            layerStage.drawImage(cache, inst.X - 64, inst.Y - 64)
+            if (inst.hide) {
+                layerStage.drawImage(hide, inst.X - 64, inst.Y - 64)
+            } else {
+                layerStage.drawImage(cache, inst.X - 64, inst.Y - 64)
+            }
         }
     });
     return inst
